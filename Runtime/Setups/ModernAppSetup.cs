c0b5bf60b4d69d989c96d587f3ae68befae5a166
Nikita Romanov <romanow.n.e@icloud.com>
2025-05-23 02:24:57 +0300

    fixes

--- Runtime/Setups/ModernAppSetup.cs
+++ Runtime/Setups/ModernAppSetup.cs
@@ -2,14 +2,12 @@
 using Plugins.Modern.DI.Units;
 using System;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Plugins.Modern.DI.Setups {
 	[CreateAssetMenu(menuName = "AppSetup", fileName = "AppSetup")]
 	public class ModernAppSetup : ScriptableObject {
 		public ModernComponentsRegistry modernComponentsRegistry { get; } = new();
-
-		[FormerlySerializedAs("units")]
+		
 		[SerializeField]
 		private ModernAppUnit[] _units;
 		
